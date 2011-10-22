e1b9341ef490c096677479f49e704cca4ee174e9
Ståle Kristoffersen <staalebk@ifi.uio.no>
2011-10-23 01:33:10 +0200

    Wall avoidance.

--- src/Ant.java
+++ src/Ant.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -46,7 +48,35 @@ public class Ant {
 	}
 	
 	public List<Aim> avoidWall(){
-		return null;
+		List<Aim> allDirections = Arrays.asList(Aim.values());
+		List<Aim> canMove = new ArrayList<Aim>();
+		List<Aim> cantMove = new ArrayList<Aim>();
+		List<Aim> willMove = new ArrayList<Aim>();
+ 		for(Aim direction : allDirections){
+			if(Util.canMoveDirection(this.position, direction)){
+				canMove.add(direction);
+			} else {
+				cantMove.add(direction);
+			}
+		}
+		if(cantMove.contains(Aim.EAST) && canMove.contains(Aim.SOUTH)){
+			willMove.add(Aim.SOUTH); 
+		} else if (cantMove.contains(Aim.NORTH) && canMove.contains(Aim.EAST)){
+			willMove.add(Aim.EAST);
+		} else if (cantMove.contains(Aim.WEST) && canMove.contains(Aim.NORTH)){
+			willMove.add(Aim.NORTH);
+		} else if (cantMove.contains(Aim.SOUTH) && canMove.contains(Aim.WEST)){
+			willMove.add(Aim.WEST);
+		} else if (canMove.size() == 1) {
+			willMove = canMove;
+		}
+		return willMove;
+	}
+	
+	public List<Aim> isStuck(){
+		this.stuckAt = this.getPosition();
+		this.wallAvoidanceMode = true;
+		return this.avoidWall();
 	}
 	
 	public List<Aim> makeMovementDecision() {
