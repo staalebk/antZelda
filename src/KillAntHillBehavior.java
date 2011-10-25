883fa8caf60454e50ccec7f22374dfa121a42631
Espen Haviken <espen.haviken@gmail.com>
2011-10-25 21:14:44 +0200

    Dont attack anthill if we have less then 10 ants

--- src/KillAntHillBehavior.java
+++ src/KillAntHillBehavior.java
@@ -8,6 +8,12 @@ public class KillAntHillBehavior extends Behavior {
 
 	@Override
 	public BehaviorDecision move() {
+		
+		// Ignore enemy anthills if we are under 10 ants alive. Better get some food before we continue...
+		if(MyBot.antPop.size() < 10) {
+			return BehaviorDecision.NO_DECISION;
+		}
+		
 		Tile anthill = Util.getClosestTile(owner.getPosition(), MyBot.enemyAntHills);		
 
 		if(anthill != null) {
