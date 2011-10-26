3b12e9be465420c699bdaa426737998f323a4020
Ståle Kristoffersen <staalebk@ifi.uio.no>
2011-10-26 21:38:03 +0200

    Fixed A*

--- src/AStarTile.java
+++ src/AStarTile.java
@@ -19,9 +19,13 @@ public class AStarTile extends AStar<Tile> {
 	@Override
 	protected Double g(Tile from, Tile to) {
 		if(MyBot.ants.getMyHills().contains(to)) {
-			return Double.MAX_VALUE;
+			return 40.0;
+		}else if(!MyBot.seenTiles[to.getRow()][to.getCol()]){
+			return 5.0;
+		}else if(Util.samePosition(to, from)){
+			return 0.0;
 		}
-		return (double)MyBot.ants.getDistance(from, to);
+		return 1.0;
 	}
 
 	@Override
@@ -34,8 +38,12 @@ public class AStarTile extends AStar<Tile> {
 		List<Aim> allDirections = Arrays.asList(Aim.values());
 		List<Tile> canMove = new ArrayList<Tile>();
  		for(Aim direction : allDirections){
+ 			Tile to = MyBot.ants.getTile(node, direction);
 			if(Util.canMoveDirection(node, direction)){
-				canMove.add(MyBot.ants.getTile(node, direction));
+				canMove.add(to);
+			}
+			if(!MyBot.seenTiles[to.getRow()][to.getCol()]){
+				canMove.add(to);
 			}
  		}
 		return canMove;
